29df2986abfd0be064429e92ea932f712f05fc97
ahaurw01 <aaron.haurwitz@gmail.com>
2015-12-12 18:21:29 -0500

    add production config file for migrations

---
+++ migrations/config/production.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  firebase: {
+    url: 'https://dazzling-fire-7827.firebaseio.com/'
+  },
+  webatrice: {
+    imageUrl: 'https://big-furry-monster.herokuapp.com/images/'
+  }
+};
